7f1e1243fd306e2289a1b6dadc2c3340c41022b7
Yash Dnyaneshwar Shelke <yashshelke885@gmail.com>
2024-08-19 10:31:35 +0530

    style fixes

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import useScrollAnimation from "../hooks/useScrollAnimation";
 import { notifyParticipate } from "./notify";
+import unstop from '../sponsors/unstop.webp'
 
 const EventCard = ({ event, index }) => {
 	const { ref, controls } = useScrollAnimation(
@@ -13,7 +14,7 @@ const EventCard = ({ event, index }) => {
 	return (
 		<motion.div
 			ref={ref}
-			className="border border-gray-700 backdrop-filter backdrop-blur-md rounded-lg shadow-lg overflow-hidden transition duration-300"
+			className="border grid border-gray-700 backdrop-filter backdrop-blur-md rounded-lg shadow-lg overflow-hidden transition duration-300"
 			initial={{ opacity: 0, y: 50 }}
 			animate={controls}
 			whileHover={{ scale: 1.03 }}
@@ -29,7 +30,7 @@ const EventCard = ({ event, index }) => {
 					transition={{ delay: 0.2 }}
 				/>
 				<motion.div
-					className="p-4"
+					className="p-4 flex flex-col justify-between"
 					initial={{ opacity: 0 }}
 					animate={controls}
 					transition={{ delay: 0.4 }}
@@ -37,17 +38,43 @@ const EventCard = ({ event, index }) => {
 					<h3 className="text-xl text-gray-300 font-bold mb-2">
 						{event.title}
 					</h3>
-					<p className="capitalize text-white mb-4">
+					<p className="capitalize text-white">
 						Prize Pool 🏆:{" "}
 						<span className="text-green-500 font-bold">{event.price.pool}</span>
 					</p>
+
 					<p className="capitalize text-blue-300 mb-4">
 						{event.category} Event
 					</p>
 					<p className="text-gray-400 mb-4">{event.description}</p>
-					<button className="text-white border-b bg-gray-800 rounded-lg p-2">
-						See Details
-					</button>
+					{event.id === 1 ? (
+						<div className="flex absolute right-4 items-center gap-2 mb-3">
+							<div>
+									<span className="text-gray-400 bg-clip-text text-sm text-transparent bg-gradient-to-r from-gray-500 to-white">
+										powered by
+									</span>
+							</div>
+							<motion.div
+								initial={{ opacity: 0 }}
+								animate={{ opacity: 1 }}
+								transition={{ delay: 1, duration: 0.8 }}
+								className="inline-block"
+							>
+								<img
+									src={unstop}
+									alt="Unstop"
+									className="h-7 rounded-full animate-pulse"
+								/>
+							</motion.div>
+						</div>
+					) : (
+						""
+					)}
+					<div className="grid">
+						<button className="text-white border-b bg-gray-800 rounded-lg p-2">
+							See Details
+						</button>
+					</div>
 				</motion.div>
 			</Link>
 		</motion.div>
--- src/components/Events.js
+++ src/components/Events.js
@@ -3,7 +3,6 @@ import { motion } from "framer-motion";
 import useScrollAnimation from "../hooks/useScrollAnimation";
 import EventCard from "./EventCard";
 import GearIcon from "./GearIcon";
-import unstop from "../sponsors/unstop.webp"
 
 import eventData from "./eventData";
 import { notifyEvent } from "./notify";
@@ -21,28 +20,9 @@ const Events = () => {
 		<section ref={ref} className="bg-black py-24">
 			<GearIcon size={730} initialRotation={0} position={1} />
 			<div className="container mx-auto px-4 md:px-0">
-				<div className="flex items-center sm:gap-5 gap-2 py-6">
-					<div>
-						<motion.h2 className="sm:text-3xl text-2xl inli font-bold text-gray-300 font-mono">
+						<motion.h2 className="py-4 text-3xl font-bold text-gray-300 font-mono">
 							Events{" "}
-							<span className="text-gray-400 bg-clip-text text-transparent font-bold bg-gradient-to-r from-gray-500 to-white">
-								powered by
-							</span>
 						</motion.h2>
-					</div>
-					<motion.div
-						initial={{ opacity: 0 }}
-						animate={{ opacity: 1 }}
-						transition={{ delay: 1, duration: 0.8 }}
-						className="inline-block my-auto"
-					>
-						<img
-							src={unstop}
-							alt="Unstop"
-							className="sm:h-8 h-7 rounded-full animate-pulse"
-						/>
-					</motion.div>
-				</div>
 				<div className="grid grid-cols-1 md:grid-cols-2 gap-12">
 					{eventData.map((event, eventIndex) => (
 						<EventCard
